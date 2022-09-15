8aab082cae5501642fba975e56c22465b8e06618
Talha <36957144+jibran1298@users.noreply.github.com>
2022-09-15 16:10:41 +0500

    :construction: Redux

--- src/redux/auth/authReducer.js
+++ src/redux/auth/authReducer.js
@@ -1,21 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { api } from '../services/api'
+
+const userLogin = createAsyncThunk(
+  'users/fetchByIdStatus',
+  async (thunkAPI) => {
+    const response = await api.useLoginQuery()
+    return response.data
+  }
+)
 
 const initialState = {
-    accessToken: '',
-    userDetails: {}
+  accessToken: '',
+  userDetails: {},
+  data: {},
 }
 
 export const auth = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        addAccessToken: (state, action) => {
-            state.accessToken += action.payload
-        },
-        addUserDetails: (state, action) => {
-            state.userDetails += action.payload
-        },
+  name: 'auth',
+  initialState,
+  reducers: {
+    addAccessToken: (state, action) => {
+      state.accessToken += action.payload
     },
+    addUserDetails: (state, action) => {
+      state.userDetails += action.payload
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(userLogin.fulfilled, (state, action) => {
+      state.data = action.payload
+    })
+  },
 })
 
 // Action creators are generated for each case reducer function
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import { auth } from './auth/authReducer'
 
 export const store = configureStore({
-    reducer: {
-        auth: auth
-    },
+  reducer: {
+    auth: auth,
+  },
 })
